f553ef59a7a562bef7cb94e3e7a1915e006e2d86
aslankaan <a.kaanyilmaz@windowslive.com>
2016-11-29 11:03:50 +0300

    Splashscreen and Icon

--- src/pages/wi-fi/wi-fi.ts
+++ src/pages/wi-fi/wi-fi.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import * as WifiWizard from '../../../plugins/com.pylonproducts.wifiwizard/www/WifiWizard.js';
 
+
 /*
   Generated class for the WiFi page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
-let that;
+let wifi_list;
+let connected: boolean = false;
 
 @Component({
   selector: 'page-wi-fi',
@@ -15,29 +17,51 @@ let that;
 })
 export class WiFiPage {
   info: any;
-
+  show_spinner = false;
+  connected: boolean = false;
   constructor() {
+    
   }
 
 
-  fail(e){
+
+  getScanResult() {
+     WifiWizard.getScanResults(this.scan_success, this.scan_fail);
+     this.show_spinner = true; 
+        setTimeout(()=>{
+          this.info = wifi_list;
+          this.show_spinner = false; 
+        },1000);
+
+  }
+  connectNetwork(ssid, wifiPassword){
+    console.log(ssid + " " + wifiPassword)
+    let wifi: any = WifiWizard.formatWifiConfig(ssid, wifiPassword, "WPA"); 
+    WifiWizard.addNetwork(wifi, this.connect_success, this.connect_fail);
+    WifiWizard.connectNetwork(ssid, this.connect_success, this.connect_fail);
+    if(connected){
+      this.connected = true;
+    }else{
+      this.connected = false;
+    }
+  }
+
+  scan_fail(e){
     console.log("Failed"+e);
   }
 
-  success(a){
+  scan_success(a){
     console.log("List: "+a);
-    that = a;
+    wifi_list = a;
   }
 
-  startScan() { 
-    WifiWizard.startScan(
-      ()=>{console.log("Scan Started");},
-      ()=>{console.log("Scan Failed");}
-    );
+  connect_fail(e){
+    console.log("Connection failed.");
+    connected = false;
   }
-
-  getScanResult() {
-     WifiWizard.getScanResults(this.success, this.fail);
-     this.info = that;
+  connect_success(a){
+    console.log("Connected!");
+    connected = true;
   }
 }
+
